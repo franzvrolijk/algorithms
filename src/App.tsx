dd177aec5ef77af225a8e4f0e1bff83355d85aab
Franz Vrolijk <franzvrolijk@gmail.com>
2022-11-06 19:21:08 +0100

    made more suitable for 1080p

--- src/App.tsx
+++ src/App.tsx
@@ -22,48 +22,52 @@ function App() {
   return (
     <div style={{ display: "flex", flexDirection: "column", alignItems: "center", height: "100vh", justifyContent: "space-between" }}>
       <div className="settings">
-        <h1 style={{color: colors.dark}}>Algorithm</h1>
-        <select className="select" onChange={(e) => setfunctionName(e.target.value)}>
-          <option value="mergeSort">Merge sort</option>
-          <option value="quickSort">Quick sort</option>
-          <option value="someSort">Some sort</option>
-        </select>
-        <h1 style={{ color: colors.dark }}>Visualization delay</h1>
-        <input
-          min={0}
-          max={Math.floor(Math.sqrt(1000))}
-          className="input"
-          defaultValue={8}
-          onChange={(e) => {
-            tickMs = Math.pow(e.target.valueAsNumber, 2);
-          }}
-          id="speed"
-          type="range"
-        />
-        <h1 style={{ color: colors.dark }}>Number of elements</h1>
-        <input
-          min={2}
-          max={Math.floor(Math.sqrt(1000))}
-          className="input"
-          defaultValue={Math.sqrt(array.length)}
-          onChange={(e) => {
-            setArray(createElementArray(Math.pow(e.target.valueAsNumber, 2)));
-          }}
-          id="speed"
-          type="range"
-        />
-        <h1>Smoothing</h1>
-        <input
-          min={0}
-          max={9}
-          className="input"
-          defaultValue={smoothing}
-          onChange={(e) => {
-            setSmoothing(e.target.valueAsNumber);
-          }}
-          id="smoothing"
-          type="range"
-        />
+        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+          <div style={{ alignItems: "center", display: "flex", flexDirection: "column",marginBottom: "2vh" }}>
+            <h1 style={{ color: colors.dark, textAlign: "center" }}>Algorithm</h1>
+            <select className="select" onChange={(e) => setfunctionName(e.target.value)}>
+              <option value="mergeSort">Merge sort</option>
+              <option value="quickSort">Quick sort</option>
+              <option value="someSort">Some sort</option>
+            </select>
+          </div>
+          <h2 style={{ color: colors.dark }}>Visualization delay</h2>
+          <input
+            min={0}
+            max={Math.floor(Math.sqrt(1000))}
+            className="input"
+            defaultValue={8}
+            onChange={(e) => {
+              tickMs = Math.pow(e.target.valueAsNumber, 2);
+            }}
+            id="speed"
+            type="range"
+          />
+          <h2 style={{ color: colors.dark }}>Number of elements</h2>
+          <input
+            min={2}
+            max={Math.floor(Math.sqrt(300))}
+            className="input"
+            defaultValue={Math.sqrt(array.length)}
+            onChange={(e) => {
+              setArray(createElementArray(Math.pow(e.target.valueAsNumber, 2)));
+            }}
+            id="speed"
+            type="range"
+          />
+          <h2>Smoothing</h2>
+          <input
+            min={0}
+            max={9}
+            className="input"
+            defaultValue={smoothing}
+            onChange={(e) => {
+              setSmoothing(e.target.valueAsNumber);
+            }}
+            id="smoothing"
+            type="range"
+          />
+        </div>
         <div>
           <button id="shuffle" className="button" onClick={(e) => set(createElementArray(array.length))}>
             Shuffle elements
@@ -73,14 +77,15 @@ function App() {
           </button>
         </div>
       </div>
-      <div style={{ display: "flex", width: "80%", marginBottom: "0px", bottom: 0, alignItems: "flex-end" }}>
+      <div style={{ display: "flex", width: "80%", marginBottom: "0px", bottom: 0, alignItems: "flex-end", height: "50vh" }}>
         {array.map((el, i) => (
           <div
             key={i}
             style={{
               border: `solid ${colors.light} 3px`,
+              borderBottom: 0,
               backgroundColor: `${el.isDone ? colors.green : el.primary ? colors.red : el.secondary ? colors.turquoise : el.tertiery ? colors.peach : colors.dark}`,
-              height: `${el.value * 7}px`,
+              height: `${el.value / 2}vh`,
               width: `${(1 / array.length) * 100}%`,
               transition: `0.${smoothing}s`,
             }}
